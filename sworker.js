fa9e2dd612934164ba3e33f4ffd82f823bc043d3
Subodh Garg <gargsubodh36@gmail.com>
2017-12-14 02:48:55 +0530

    reverting changes from master branch

--- sworker.js
+++ sworker.js
@@ -1,16 +1,12 @@
 self.addEventListener('fetch', function(event){
-// FOLLOWING CODE NEEDS TO BE PART OF SOME OTHER JS SCRIPT CODE(ANY JS FILE OR CODE B/W SCRIPT TAG) WHICH
-// YOU ARE SURE WILL BE EXECUTED.
-// I MOVED IT BETWEEN <SCRIPT> TAG IN YOUR INDEX.HTML FILE.
-
-// if('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//   .register('sworker.js')
-//   .then(function() {
-//     console.log("Service Worker registered successfully");
-//   })
-//   .catch(function() {
-//     console.log("Service worker registration failed")
-//   });
-// }
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('sworker.js')
+  .then(function() {
+    console.log("Service Worker registered successfully");
+  })
+  .catch(function() {
+    console.log("Service worker registration failed")
+  });
+}
 });
