7009619f5f68106532c63a87ce77763098c3c4b9
Subodh Garg <gargsubodh36@gmail.com>
2017-12-14 02:50:10 +0530

    Web App Manifest's service worker registering issue resolved

--- sworker.js
+++ sworker.js
@@ -1,12 +1,15 @@
 self.addEventListener('fetch', function(event){
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-  .register('sworker.js')
-  .then(function() {
-    console.log("Service Worker registered successfully");
-  })
-  .catch(function() {
-    console.log("Service worker registration failed")
-  });
-}
+  // FOLLOWING CODE NEEDS TO BE PART OF SOME OTHER JS SCRIPT CODE(ANY JS FILE OR CODE B/W SCRIPT TAG) WHICH
+  // YOU ARE SURE WILL BE EXECUTED.
+  // I MOVED IT BETWEEN <SCRIPT> TAG IN YOUR INDEX.HTML FILE.
+// if('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//   .register('sworker.js')
+//   .then(function() {
+//     console.log("Service Worker registered successfully");
+//   })
+//   .catch(function() {
+//     console.log("Service worker registration failed")
+//   });
+// }
 });
